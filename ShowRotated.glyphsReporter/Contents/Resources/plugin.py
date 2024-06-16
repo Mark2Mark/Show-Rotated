2bc5152640cbcc799d7331e48fe260135eb4bf70
Mark Frömberg <mirque84@gmail.com>
2024-06-16 16:16:02 +0200

    more refactoring

--- ShowRotated.glyphsReporter/Contents/Resources/plugin.py
+++ ShowRotated.glyphsReporter/Contents/Resources/plugin.py
@@ -213,124 +213,115 @@ class ShowRotated(ReporterPlugin):
             return
 
         try:
-            dash_count_phase = [4 / self.getScale(), 4 / self.getScale()], 2, 0
             bounds = bezier_path.bounds()
+            if self.slider_menu_view.group.checkbox_selection_mode.get():
+                selected_paths = NSBezierPath.alloc().init()
+                for p in self.selected_paths(layer):
+                    selected_paths.appendBezierPath_(p.bezierPath)
+                bezier_path = selected_paths
+                bounds = selected_paths.bounds()
 
-            if self.slider_menu_view.group.checkbox_selection_mode.get() == True:
-                try:
-                    selected_paths = NSBezierPath.alloc().init()
-                    for p in self.selected_paths(layer):
-                        selected_paths.appendBezierPath_(p.bezierPath)
-                        bounds = selected_paths.bounds()
-                    bezier_path = selected_paths
-                except:
-                    pass
-            else:
-                bezier_path = layer.completeBezierPath
-                bounds = layer.bounds
-
-            x = bounds.origin.x + 0.5 * bounds.size.width
-            y = bounds.origin.y + 0.5 * bounds.size.height
-
-            rotation = NSAffineTransform.transform()
-            rotation.translateXBy_yBy_(x, y)
-            rotation.rotateByDegrees_(angle)
-            rotation.translateXBy_yBy_(-x, -y)
-            bezier_path.transformUsingAffineTransform_(rotation)
-            bezier_path_rotated_only = bezier_path.copy()
-
-            do_flip_horizontal = self.slider_menu_view.group.horizontal.get()
-            do_flip_vertical = self.slider_menu_view.group.vertical.get()
-
-            cross_size = 10 / self.getScale()
-            center_cross_1 = NSBezierPath.alloc().init()
-            center_cross_2 = NSBezierPath.alloc().init()
-            center_cross_1.moveToPoint_((x, y - cross_size))
-            center_cross_2.moveToPoint_((x + cross_size, y))
-            center_cross_1.lineToPoint_((x, y + cross_size))
-            center_cross_2.lineToPoint_((x - cross_size, y))
-            center_cross_1.setLineWidth_(0.75 / self.getScale())
-            center_cross_2.setLineWidth_(0.75 / self.getScale())
-
-            mirror_line = NSBezierPath.alloc().init()
-            mirror_line.setLineWidth_(0.75 / self.getScale())
-
-            # Apply flipping transformations
-            flip_transform = NSAffineTransform.transform()
-            flip_transform.translateXBy_yBy_(x, y)
-
-            if do_flip_horizontal:
-                flip_transform.scaleXBy_yBy_(-1, 1)
-                mirror_line.moveToPoint_((x, NSMaxY(bounds)))
-                mirror_line.lineToPoint_((x, NSMinY(bounds)))
-                mirror_line.transformUsingAffineTransform_(self.rotation(x, y, 0))
-                mirror_line.setLineDash_count_phase_(*dash_count_phase)
-                mirror_line.stroke()
-            if do_flip_vertical:
-                flip_transform.scaleXBy_yBy_(1, -1)
-                mirror_line.moveToPoint_((NSMinX(bounds), NSMidY(bounds)))
-                mirror_line.lineToPoint_((NSMaxX(bounds), NSMidY(bounds)))
-                mirror_line.transformUsingAffineTransform_(self.rotation(x, y, 0))
-                mirror_line.setLineDash_count_phase_(*dash_count_phase)
-                mirror_line.stroke()
-
-            flip_transform.translateXBy_yBy_(-x, -y)
-
-            if do_flip_horizontal or do_flip_vertical:
-                bezier_path.transformUsingAffineTransform_(flip_transform)
-
-            # Draw flipped path
-            if bezier_path:
-                bezier_path_rotated_only.setLineDash_count_phase_(*dash_count_phase)
-                bezier_path_rotated_only.setLineWidth_(1 / self.getScale())
-                bezier_path_rotated_only.stroke()
-                bezier_path.fill()
-                center_cross_1.stroke()
-                center_cross_2.stroke()
-
-            # Draw a rectangle with gradient fill
-            gradient_rect_horizontal = NSZeroRect
-            gradient_rect_vertical = NSZeroRect
-            thickness = 100
-            mirror_color = NSColor.colorWithDeviceRed_green_blue_alpha_(
-                *self.color[:3], 0.2
-            )
-            mirror_color_clear = NSColor.clearColor()
-            if do_flip_horizontal or do_flip_vertical:
-                if do_flip_horizontal:
-                    gradient_rect_horizontal = NSMakeRect(
-                        x, NSMinY(bounds), thickness, NSHeight(bounds)
-                    )
-                    gradient_path_horizontal = NSBezierPath.bezierPathWithRect_(
-                        gradient_rect_horizontal
-                    )
-                    gradient_horizontal = NSGradient.alloc().initWithColors_(
-                        [
-                            mirror_color,
-                            mirror_color_clear,
-                        ]
-                    )
-                    gradient_horizontal.drawInBezierPath_angle_(
-                        gradient_path_horizontal, 0
-                    )
-                if do_flip_vertical:
-                    gradient_rect_vertical = NSMakeRect(
-                        NSMinX(bounds), NSMidY(bounds), NSWidth(bounds), thickness
-                    )
-                    gradient_path_vertical = NSBezierPath.bezierPathWithRect_(
-                        gradient_rect_vertical
-                    )
-                    gradient_vertical = NSGradient.alloc().initWithColors_(
-                        [
-                            mirror_color,
-                            mirror_color_clear,
-                        ]
-                    )
-                    gradient_vertical.drawInBezierPath_angle_(
-                        gradient_path_vertical, 90
-                    )
+            x, y = self.get_center(bounds)
+            self.transform_path(bezier_path, x, y, angle)
+            self.apply_flip_transformations(bezier_path, bounds, x, y)
+            self.draw_path(bezier_path, bounds, x, y)
         except:
             print(traceback.format_exc())
+
+    def get_center(self, bounds):
+        x = bounds.origin.x + 0.5 * bounds.size.width
+        y = bounds.origin.y + 0.5 * bounds.size.height
+        return x, y
+
+    def transform_path(self, bezier_path, x, y, angle):
+        rotation = self.rotation(x, y, angle)
+        bezier_path.transformUsingAffineTransform_(rotation)
+
+    def apply_flip_transformations(self, bezier_path, bounds, x, y):
+        flip_transform = NSAffineTransform.transform()
+        flip_transform.translateXBy_yBy_(x, y)
+
+        if self.slider_menu_view.group.horizontal.get():
+            flip_transform.scaleXBy_yBy_(-1, 1)
+            self.draw_mirror_line(x, NSMaxY(bounds), x, NSMinY(bounds), x, y, 0)
+        if self.slider_menu_view.group.vertical.get():
+            flip_transform.scaleXBy_yBy_(1, -1)
+            self.draw_mirror_line(
+                NSMinX(bounds), NSMidY(bounds), NSMaxX(bounds), NSMidY(bounds), x, y, 0
+            )
+
+        flip_transform.translateXBy_yBy_(-x, -y)
+        if (
+            self.slider_menu_view.group.horizontal.get()
+            or self.slider_menu_view.group.vertical.get()
+        ):
+            bezier_path.transformUsingAffineTransform_(flip_transform)
+
+    def draw_mirror_line(self, x1, y1, x2, y2, x, y, angle):
+        mirror_line = NSBezierPath.alloc().init()
+        mirror_line.setLineWidth_(0.75 / self.getScale())
+        mirror_line.moveToPoint_((x1, y1))
+        mirror_line.lineToPoint_((x2, y2))
+        mirror_line.transformUsingAffineTransform_(self.rotation(x, y, angle))
+        mirror_line.setLineDash_count_phase_(
+            [4 / self.getScale(), 4 / self.getScale()], 2, 0
+        )
+        mirror_line.stroke()
+
+    def draw_path(self, bezier_path, bounds, x, y):
+        if bezier_path:
+            bezier_path.setLineDash_count_phase_(
+                [4 / self.getScale(), 4 / self.getScale()], 2, 0
+            )
+            bezier_path.setLineWidth_(1 / self.getScale())
+            bezier_path.stroke()
+            bezier_path.fill()
+            self.draw_center_cross(x, y)
+            self.draw_gradient(bounds, x, y)
+
+    def draw_center_cross(self, x, y):
+        cross_size = 10 / self.getScale()
+        center_cross_1 = NSBezierPath.alloc().init()
+        center_cross_2 = NSBezierPath.alloc().init()
+        center_cross_1.moveToPoint_((x, y - cross_size))
+        center_cross_2.moveToPoint_((x + cross_size, y))
+        center_cross_1.lineToPoint_((x, y + cross_size))
+        center_cross_2.lineToPoint_((x - cross_size, y))
+        center_cross_1.setLineWidth_(0.75 / self.getScale())
+        center_cross_2.setLineWidth_(0.75 / self.getScale())
+        center_cross_1.stroke()
+        center_cross_2.stroke()
+
+    def draw_gradient(self, bounds, x, y):
+        do_flip_horizontal = self.slider_menu_view.group.horizontal.get()
+        do_flip_vertical = self.slider_menu_view.group.vertical.get()
+        thickness = 100
+        mirror_color = NSColor.colorWithDeviceRed_green_blue_alpha_(
+            *self.color[:3], 0.2
+        )
+        mirror_color_clear = NSColor.clearColor()
+        if do_flip_horizontal:
+            gradient_rect_horizontal = NSMakeRect(
+                x, NSMinY(bounds), thickness, NSHeight(bounds)
+            )
+            gradient_path_horizontal = NSBezierPath.bezierPathWithRect_(
+                gradient_rect_horizontal
+            )
+            gradient_horizontal = NSGradient.alloc().initWithColors_(
+                [mirror_color, mirror_color_clear]
+            )
+            gradient_horizontal.drawInBezierPath_angle_(gradient_path_horizontal, 0)
+        if do_flip_vertical:
+            gradient_rect_vertical = NSMakeRect(
+                NSMinX(bounds), NSMidY(bounds), NSWidth(bounds), thickness
+            )
+            gradient_path_vertical = NSBezierPath.bezierPathWithRect_(
+                gradient_rect_vertical
+            )
+            gradient_vertical = NSGradient.alloc().initWithColors_(
+                [mirror_color, mirror_color_clear]
+            )
+            gradient_vertical.drawInBezierPath_angle_(gradient_path_vertical, 90)
 
     @objc.python_method
     def update(self, sender):
